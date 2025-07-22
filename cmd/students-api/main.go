5c61f69d4bf944b4fba783c80f6a27e1e34b6f3f
Naman Sharma <namsh70747@gmail.com>
2025-07-23 04:59:19 +0530

    Gracefull Shutdown Handling

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
+	"github.com/namsh70747/Rest_API/internal/config"
+)
+
+// load config
 func main() {
-	fmt.Println("Nmman Sharma ")
+	cfg := config.MustLoad()
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to students API"))
+	})
+
+	//database setup
+	//setup router
+
+	//setup server
+	server := http.Server{
+		Addr:    cfg.HTTPServer.Addr, // Use the address from config
+		Handler: router,
+	}
+
+	fmt.Printf("server started %s", cfg.HTTPServer.Addr)
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done,os.Interrupt)
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("failed to start server")
+		}
+	}()
+
+	<-done
+	slog.Info("shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err := server.Shutdown(ctx)
+	if err != nil {
+		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
+
+	}
 }
